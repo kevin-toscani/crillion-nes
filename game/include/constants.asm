7fcc2ffe5eafa6634f1c66a412220d410b8915ef
Kevin81 <kevin.van.der.burg@gmail.com>
2023-02-15 18:56:19 +0100

    Added game specific variables and constants

--- game/include/constants.asm
+++ game/include/constants.asm
@@ -1,1 +1,6 @@
-;; additional game-specific constants go here
+;; Ball bounding box constants
+BALL_HEIGHT  = #$0A  ; Height of the ball graphic within the sprite(s)
+BALL_WIDTH   = #$0A  ; Width of the ball graphic within the sprite(s)
+BALL_TOP     = #$03  ; Top offset of the ball graphic
+BALL_LEFT    = #$03  ; Left offset of the ball graphic
+
--- game/ram/zp.asm
+++ game/ram/zp.asm
@@ -1,1 +1,17 @@
-;; game-specific zero-page variables go here
+;; Ball variables
+ball_xpos    .db 1   ; left pixel x-position on screen
+ball_ypos    .db 1   ; top pixel y-position on screen
+ball_lives   .db 1   ; number of lives left
+ball_score   .db 5   ; decimal score, five bytes (0-9)
+ball_bonus   .db 3   ; decimal bonus points, three bytes (0-9)
+
+;; Ball flags
+;; #% c c c l h v n a
+;;    | | | | | | | +-- ball is dead (0) or alive (1)
+;;    | | | | | | +---- ball is being nudged (1) or not (0)
+;;    | | | | | +------ ball moves up (0) or down (1)
+;;    | | | | +-------- ball is moving horizontally (1) or not (0)
+;;    | | | +---------- ball is moving left (0) or right (1)
+;;    +-+-+------------ ball color (1-6)
+ball_flags   .db 1
+
