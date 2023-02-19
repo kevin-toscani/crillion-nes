5102c35e4cd809955823a5c064e2d3f6fa35bba2
Kevin81 <kevin.van.der.burg@gmail.com>
2023-02-19 11:53:27 +0100

    Added Mesen/Log loader to build file

--- build.bat
+++ build.bat
@@ -1,1 +1,8 @@
-@d:\nesdev\tools\asm6\asm6 crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
+@echo off
+d:\nesdev\tools\asm6\asm6 crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
+if exist output\crillion.nes (
+    "C:\Program Files (x86)\Mesen\Mesen.exe" output\crillion.nes
+) else (
+    notepad output\crillion.log
+)
+echo on
