b7008ddeb1922cb53e47755f7e9af3cdf1f57fbb
Kevin81 <kevin.van.der.burg@gmail.com>
2023-03-01 00:56:09 +0100

    Replaced asm6 with asm6f

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-d:\nesdev\tools\asm6\asm6 crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
+asm6f -m crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
 if exist output\crillion.nes (
     "C:\Program Files (x86)\Mesen\Mesen.exe" output\crillion.nes
 ) else (
