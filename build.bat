8c82f0f8d01fe50a6e6e464eb0db35bf9fa650d8
Kevin81 <kevin.van.der.burg@gmail.com>
2023-03-14 00:02:12 +0100

    Removed mapping from build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-asm6f -m crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
+asm6f crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
 if exist output\crillion.nes (
     "C:\Program Files (x86)\Mesen\Mesen.exe" output\crillion.nes
 ) else (
