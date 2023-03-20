ad11a66ab72dd22fea403b4e513157800ab50e95
Kevin81 <kevin.van.der.burg@gmail.com>
2023-03-20 23:09:10 +0100

    Added Mesen label file generator

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 asm6f crillion.asm output\crillion.nes output\crillion.txt 1>output\crillion.log 2>&1
 if exist output\crillion.nes (
+    php mklabels.php
     "C:\Program Files (x86)\Mesen\Mesen.exe" output\crillion.nes
 ) else (
     notepad output\crillion.log
---
+++ mklabels.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ *  mklabels.php
+ *
+ *  Primitive script to generate a Mesen label file (.mlb) from asm6 demo output
+ *
+ **/
+ 
+ 
+ 
+// Set default labels (or, rather, label)
+$labels = [
+    'R:0200:sprite_ram',
+];
+
+// Get demo output
+$file = file_get_contents('output/crillion.txt');
+$lines = explode("\n", $file);
+
+// Go through all lines
+foreach($lines as $line) {
+    
+    // Find RAM labels
+    if(preg_match("/^0(0[0-9A-F]{3})\s+([a-zA-Z0-9_]+)\s+\.dsb ([0-9]+)/", $line, $match)) {
+        list($void, $address, $label, $length) = $match;
+        if($length == 1) {
+            $labels[] = 'R:' . $address . ':' . $label;
+            continue;
+        }
+
+        $address_end = strtoupper(str_pad(dechex(hexdec($address) + $length - 1), 4, '0', STR_PAD_LEFT));
+
+        $labels[] = 'R:' . $address . '-' . $address_end . ':' . $label;
+        continue;
+    }
+    
+    // Find subroutines, tables and level data
+    if(preg_match("/^0([0-9A-F]{4})\s+(sub|tbl|lvl)_([a-zA-Z0-9_]+):/", $line, $match)) {
+        $labels[] = 'G:' . $match[1] . ':' . $match[2] . '_' . $match[3];
+    }
+    
+    // Find interrupt addresses
+    if(preg_match("/^0([0-9A-F]{4})\s+(RESET|IRQ|NMI):/", $line, $match)) {
+        $labels[] = 'G:' . $match[1] . ':' . $match[2];
+    }
+}
+
+$fh = fopen('output/crillion.mlb', 'w');
+fwrite($fh, implode("\n", $labels) . "\n");
+fclose($fh);
